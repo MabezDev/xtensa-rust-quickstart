52e40894dafb44d2360fa3e1b7321b7f5adcd366
Scott Mabin <scott@mabez.dev>
2019-08-29 20:27:23 +0100

    Add blinky example

--- README.md
+++ README.md
@@ -6,12 +6,20 @@
 
 ### Requirements
 
-    - xargo
+    - xargo or cargo xbuild
     - xtensa-esp32-elf toolchain must be in your `$PATH`
 
 First you will need to build `rustc` and `llvm` you can find rough instructions [here](https://gist.github.com/MabezDev/26e175790f84f2f2b0f9bca4e63275d1).
 
-Modifiy the `build` script to fix the path of the custom `rustc`, then execute the build script build.
+## Starting a new project
+
+`$ cargo generate --git https://github.com/MabezDev/xtensa-rust-quickstart`
+
+To clone this repo and use it as a template.
+
+## Workflow
+
+
 
 ## Resources
 
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,58 @@
 #![no_std]
 #![no_main]
 
+use xtensa_lx6_rt as _;
+
 use core::panic::PanicInfo;
-use core::sync::atomic::{self, Ordering};
 
-use panic_ramdump as _;
+// GPIO output enable reg
+const GPIO_ENABLE_W1TS_REG: u32 = 0x3FF44024;
+
+// gpio output set register
+const GPIO_OUT_W1TS_REG: u32 = 0x3FF44008;
+// gpio output clear register
+const GPIO_OUT_W1TC_REG : u32 = 0x3FF4400C;
+
+
+const BLINKY_GPIO: u32 = 2; // the GPIO hooked up to the onboard LED
+const GPIO_FUNCX_OUT_BASE: u32 = 0x3FF44530;
+const GPIO_FUNCX_OUT_SEL_CFG: u32 = GPIO_FUNCX_OUT_BASE + (BLINKY_GPIO * 4);
+
+// const IO_MUX_GPIO2_REG: u32 = 0x3FF49040;
 
 #[no_mangle]
-fn main() {
-    
-}
+fn main() -> ! {
 
-#[doc(hidden)]
-#[no_mangle]
-#[link_section = ".iram0.vectors"]
-pub unsafe extern "C" fn Reset() -> ! {
-    // CURRENTLY DOES NOTHING
-    loop {
+    // set the pin as an output
+    unsafe {
+        core::ptr::write_volatile(GPIO_ENABLE_W1TS_REG as *mut _, 0x1 << BLINKY_GPIO);
+        core::ptr::write_volatile(GPIO_FUNCX_OUT_SEL_CFG as *mut _, 0x100); // 0x100 makes this pin a simple gpio pin - see the technical reference
         
+        // TODO select function and drive strength
+        // if your led pin doesn't default to GPIO you will need to set this register see technical ref for details
+        // core::ptr::write_volatile(IO_MUX_GPIO2_REG as *mut _, BLINKY_GPIO); // GPIO2 function 1 is being a gpio port
+    }
+    loop {
+        unsafe {
+            core::ptr::write_volatile(GPIO_OUT_W1TS_REG as *mut _, 0x1 << BLINKY_GPIO);           
+        }
+        delay(80_000_000);
+        unsafe {
+            core::ptr::write_volatile(GPIO_OUT_W1TC_REG as *mut _, 0x1 << BLINKY_GPIO);
+        }
+        delay(80_000_000);
     }
 }
 
-#[doc(hidden)]
-#[no_mangle]
-pub unsafe extern "C" fn _start() -> ! {
-    // CURRENTLY DOES NOTHING
-    loop {
-        
+pub fn delay(clocks: u32) {
+    let dummy_var: u32 = 0;
+    for _ in 0..clocks {
+        unsafe { core::ptr::read_volatile(&dummy_var) };
     }
 }
 
 
-
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
