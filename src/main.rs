14b72889a0659a7c973321b433ff64d31d9f2e9a
Scott Mabin <scott@mabez.dev>
2020-09-23 19:54:44 +0100

    Switch xtensa-lx generic crates. Add esp8266 example. Update esp32 hal.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 #![no_std]
 #![no_main]
 
-use xtensa_lx6_rt as _;
-use xtensa_lx6_rt::entry;
+use xtensa_lx_rt as _;
+use xtensa_lx_rt::entry;
 use panic_halt as _;
 
-// NOTE: building this will require you to fill out the memory.x file, see build.rs for info
+/// NOTE: These are instructions for building a custom Xtensa target, for the esp32 and esp8266
+/// see the examples/ folder. 
+///
+/// Building for a custom target:
+///     - fill out the `memory.x` file
+///     - Uncomment the build.rs
+///     - Choose a `xtensa-lx` and `xtensa-lx-rt` cpu feature
 #[entry]
 fn main() -> ! {
     loop {}
